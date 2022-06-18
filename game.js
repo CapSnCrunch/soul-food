ef033118c4bc43611a1d92c8172265e1113497e0
Samuel Perales <samuelperales4@gmail.com>
2022-06-17 23:52:09 -0500

    added mushroom

--- game.js
+++ game.js
@@ -2,6 +2,11 @@ class Game {
     constructor(state='home'){
         this.state = state
     }
+
+    update(){
+        fill(0)
+        rect(0, 0, 50, 50)
+    }
 }
 
 var game = new Game()
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+var shroom;
+function preload(){
+    shroom = loadImage('assets/mushroom.png')
+}
+
 function setup(){
     var cnv = createCanvas(window.innerWidth, window.innerHeight);
     cnv.parent('canvas');
+    imageMode(CENTER)
 }
 
+var x = y = 50
 function draw(){
     clear();
-    background(200)
+    background(255)
     noStroke()
+    image(shroom, x, y)
+    game.update()
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseDragged(){
+    x += (mouseX - x) / 5
+    y += (mouseY - y) / 5
 }
