cf41b4607c94f3b0c7c955a6ce1bde31e903fd54
Samuel Perales <samuelperales4@gmail.com>
2022-06-17 22:54:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function setup(){
+    var cnv = createCanvas(window.innerWidth, window.innerHeight);
+    cnv.parent('canvas');
+}
+
+function draw(){
+    clear();
+    background(200)
+    noStroke() 
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
